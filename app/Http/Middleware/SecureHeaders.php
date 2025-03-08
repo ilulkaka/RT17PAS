73f5064e67700ba01e294bb5d7be5702dac11715
ilulkaka <ilul.kaka.studi@gmail.com>
2025-03-08 17:05:24 +0700

    sec

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -19,8 +19,12 @@ class SecureHeaders
 
         $response = $next($request);
 
+        $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Permissions-Policy', 'geolocation=(), microphone=(), camera=(), autoplay=(), fullscreen=()');
         $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 
         return $response;
     }
