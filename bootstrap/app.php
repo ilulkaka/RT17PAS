2e1a495cb08d2abc9231aa512a9dad0239c1f47d
ilulkaka <ilul.kaka.studi@gmail.com>
2025-03-08 16:57:31 +0700

    security header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'checkability' => CheckAbility::class,
         ]);
         //$middleware->append(CheckAbility::class);
+        $middleware->append(App\Http\Middleware\SecureHeaders::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    
